b60b5052dae3ef8e01708b998c8bd94ecbec1b02
Frank Navarro <fnavarro7777@gmail.com>
2019-09-19 16:41:34 -0700

    Initiated connection to mongodb

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // App Setup
 const app = express();
