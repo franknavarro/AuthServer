49c7ebef1c9b028636c8b74b47f7b2c912b6f30a
Frank Navarro <fnavarro7777@gmail.com>
2019-09-19 15:16:21 -0700

    Started routes

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const router = require('./router');
 
 // App Setup
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
