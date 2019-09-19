9030447b4a86bbe157f5452dd1fe06947f200035
Frank Navarro <fnavarro7777@gmail.com>
2019-09-19 15:05:43 -0700

    Enabled logging through morgan

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 
 // App Setup
 const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 
 // Server Setup
 const port = process.env.PORT || 3090;
