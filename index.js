b497d162451ef91f003646d54a53fe537ffc4727
Frank Navarro <fnavarro7777@gmail.com>
2019-09-19 15:02:29 -0700

    Setup Server

---
+++ index.js
@@ -0,0 +1,14 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// App Setup
+const app = express();
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
